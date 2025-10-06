1e794b1cf313d89fbe4fc836407a45d7a96c7f9c
lorf543 <lorf543@gmail.com>
2025-10-05 21:38:37 -0400

    Fix ALLOWED_HOSTS for Railway

--- payroll/settings.py
+++ payroll/settings.py
@@ -9,7 +9,7 @@ https://docs.djangoproject.com/en/5.2/topics/settings/
 For the full list of settings and their values, see
 https://docs.djangoproject.com/en/5.2/ref/settings/
 """
-
+import dj_database_url
 from pathlib import Path
 import os
 
@@ -105,11 +105,17 @@ WSGI_APPLICATION = 'payroll.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/5.2/ref/settings/#databases
 
+# DATABASES = {
+#     'default': {
+#         'ENGINE': 'django.db.backends.sqlite3',
+#         'NAME': BASE_DIR / 'db.sqlite3',
+#     }
+# }
+
 DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
-    }
+    'default': dj_database_url.config(
+        default='postgresql://postgres:DpiorqneEWUTHdSwMCyhXmiCTAwYSHkZ@nozomi.proxy.rlwy.net:17346/railway'
+    )
 }
 
 
